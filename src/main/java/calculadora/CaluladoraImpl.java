27c3a57055e5f8dcd41e36351e86e8b86d038486
kiregon <119518904+kiregon@users.noreply.github.com>
2024-10-23 17:33:31 -0600

    Update CaluladoraImpl.java

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -5,9 +5,9 @@ public class CaluladoraImpl implements Calculadora {
 	@Override
 	public int calcular(int i1, char op, int i2) {
 		switch(op) {
-				case: '+'
+			case '+':
 				return i1 + i2;
-				case: '-'
+			case '-':
 				return i1 - i2;
 			default:
 				return 0;
