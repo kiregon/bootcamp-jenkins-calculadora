63157df8e49561ba5057813435dc13d01e5844b7
kiregon <119518904+kiregon@users.noreply.github.com>
2024-10-29 12:20:51 -0600

    Update CaluladoraImplTest.java

--- src/test/java/calculadora/CaluladoraImplTest.java
+++ src/test/java/calculadora/CaluladoraImplTest.java
@@ -21,7 +21,7 @@ class CaluladoraImplTest {
 
 	@Test
 	void test_debe_restar_cuando_se_utiliza_menos() {
-		int resultado = calculadora.calcular(0, '-', 2);
+		int resultado = calculadora.calcular(1, '-', 2);
 		Assertions.assertEquals(-1, resultado);
 	}	
 
